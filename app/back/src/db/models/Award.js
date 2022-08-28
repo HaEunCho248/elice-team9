3f09a5ec48709afd185e051d4789cf0214dbbaca
yeounbum <cd.yb0229@gmail.com>
2022-08-28 21:56:29 +0900

    User-MVP, Award-MVP, Education-MVP 기능 구현

---
+++ app/back/src/db/models/Award.js
@@ -0,0 +1,46 @@
+import { AwardModel } from "../schemas/award";
+
+class Award {
+    // Award 데이터 생성
+    static async create({ newAward }) {
+        const createNewAward = await AwardModel.create(newAward);
+        
+        return createNewAward;
+    }
+
+    // Award 데이터 가져오기
+    static async findById({ user_id }) {
+        const Award = await AwardModel.findOne({id: user_id});
+        return Award;
+    }
+
+    //Award 해당 데이터 전부 가져오기
+    static async findAll({ user_id }) {
+        const Award = await AwardModel.find({id: user_id},);
+        return Award;
+    }
+
+    // Award 데이터 업데이트
+    static async update({ _id, fieldToUpdate, newValue }){
+        const filter = { _id: _id };
+        const update = { [fieldToUpdate]: newValue };
+        const option = { returnOriginal: false };
+        const updatedAward = await AwardModel.findOneAndUpdate(
+            filter,
+            update,
+            option
+        );
+
+        return updatedAward;
+    }
+
+    // Award 데이터 삭제
+    static async delete({ _id }) {
+        const filter = { _id: _id };
+        const Award = await AwardModel.remove({ filter });
+        return Award;
+    }
+}
+
+
+export { Award };
---
+++ app/front/src/components/project/ProjectEditForm.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import * as Api from "../../api";
+import DatePicker from "react-datepicker";
+
+function ProjectEditForm({ currentProject, setProjects, setIsEditing }) {
+  //useState로 title 상태를 생성함.
+  const [title, setTitle] = useState(currentProject.title);
+  //useState로 description 상태를 생성함.
+  const [description, setDescription] = useState(currentProject.description);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // currentProject의 user_id를 user_id 변수에 할당함.
+    const user_id = currentProject.user_id;
+
+    // "projects/수상 id" 엔드포인트로 PUT 요청함.
+    await Api.put(`projects/${currentProject.id}`, {
+      user_id,
+      title,
+      description,
+      startDate, 
+      endDate,
+    });
+
+    // "projectlist/유저id" 엔드포인트로 GET 요청함.
+    const res = await Api.get("projectlist", user_id);
+    // projects를 response의 data로 세팅함.
+    setProjects(res.data);
+    // 편집 과정이 끝났으므로, isEditing을 false로 세팅함.
+    setIsEditing(false);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formBasicTitle">
+        <Form.Control
+          type="text"
+          placeholder="프로젝트 내역"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicDescription" className="mt-3">
+        <Form.Control
+          type="text"
+          placeholder="프로젝트 상세내역"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group as={Row} className="mt-3">
+        
+        <Col>
+          시작일 <DatePicker 
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          />
+        </Col>
+        <Col>
+          종료일 <DatePicker 
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          />
+        </Col>
+        
+      </Form.Group>
+
+      <Form.Group as={Row} className="mt-3 text-center mb-4">
+        <Col sm={{ span: 20 }}>
+          <Button variant="primary" type="submit" className="me-3">
+            확인
+          </Button>
+          <Button variant="secondary" onClick={() => setIsEditing(false)}>
+            취소
+          </Button>
+        </Col>
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default ProjectEditForm;
