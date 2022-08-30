db1a238753783f25f54e0e81fded0b0e618ce00a
yeounbum <cd.yb0229@gmail.com>
2022-08-30 12:02:31 +0900

    test

--- app/back/src/routers/awardRouter.js
+++ app/back/src/routers/awardRouter.js
@@ -10,7 +10,6 @@ awardRouter.post('/award/create', login_required, async function(req, res, next)
         if(is.emptyObject(req.body)) {
             throw new Error("headers의 Content-Type을 application/json으로 설정해주세요");
         }
-            
         const user_id = req.body.user_id;
         const title = req.body.title;
         const description = req.body.description;
