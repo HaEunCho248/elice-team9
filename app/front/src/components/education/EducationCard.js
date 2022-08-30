20eefcb802d90136f64bbf7d6e69a183860e6317
성현 배 <lydiabae0719@gmail.com>
2022-08-30 23:42:46 +0000

    시작, 종료 날짜 추가 및 디자인 변경 진행중

--- app/front/src/components/education/EducationCard.js
+++ app/front/src/components/education/EducationCard.js
@@ -8,10 +8,15 @@ function EducationCard({ education, isEditable, setIsEditing, setEducations }) {
       <Row className="align-items-center">
         <Col>
           <span>{education.school}</span>
+          &nbsp;&nbsp;&nbsp;
+          <span className="text-muted"> {education.position}</span>
           <br />
           <span className="text-muted">{education.major}</span>
+         
           <br />
-          <span className="text-muted">{education.position}</span>
+          <span className="text-muted">{education.startDate}</span>
+          &nbsp;&nbsp;&nbsp;
+          <span className="text-muted"> {education.endDate}</span>
         </Col>
         {isEditable && (
           <Col xs lg="1">
@@ -31,7 +36,7 @@ function EducationCard({ education, isEditable, setIsEditing, setEducations }) {
                 e.stopPropagation();
                 
                 const user_id = education.user_id;
-                const object_id = education._id;
+                const object_id = education.object_id;
                 console.log(object_id) // 디버깅 //OK
                 await Api.delete('education/delete', object_id)
                 const res = await Api.get("educationlist", user_id);
