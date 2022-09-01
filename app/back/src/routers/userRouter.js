1bea74d680a418fe02dc5efaa82b5b63bd8dcfd2
yeounbum <cd.yb0229@gmail.com>
2022-09-01 18:20:32 +0900

    좋아요 기능 미완 추가
    1. 자신의 데이터를 좋아요 할 수 있음.
    2. 다른 유저가 데이터에 접근 불가.
    3. 다른 유저가 좋아요를 눌렀을 경우에 대한 경우의 수 아를 아직 추가 안함.
    4. 아직 로직 구상중.

--- app/back/src/routers/userRouter.js
+++ app/back/src/routers/userRouter.js
@@ -107,8 +107,32 @@ userAuthRouter.put(
       const email = req.body.email ?? null;
       const password = req.body.password ?? null;
       const description = req.body.description ?? null;
-
       const toUpdate = { name, email, password, description };
+
+      // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
+      const updatedUser = await userAuthService.setUser({ user_id, toUpdate });
+
+      if (updatedUser.errorMessage) {
+        throw new Error(updatedUser.errorMessage);
+      }
+
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+userAuthRouter.put(
+  "/user/like/:id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      // URI로부터 사용자 id를 추출함.
+      const user_id = req.params.id;
+      // body data 로부터 업데이트할 사용자 정보를 추출함.
+      const isLike = req.body.isLike ?? null;
+      const toUpdate = { isLike };
 
       // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
       const updatedUser = await userAuthService.setUser({ user_id, toUpdate });
