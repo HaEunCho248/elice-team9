f39dd8c5622db0b36064f99c7ea68f507e53269e
성현 배 <lydiabae0719@gmail.com>
2022-09-01 00:40:37 +0000

    REST API 구현

--- app/front/src/components/education/EducationCard.js
+++ app/front/src/components/education/EducationCard.js
@@ -3,6 +3,18 @@ import * as Api from "../../api";
 
 function EducationCard({ education, isEditable, setIsEditing, setEducations }) {
 
+  const delHandler = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    const user_id = education.user_id;
+    const object_id = education.object_id;
+    console.log(object_id) // 디버깅 //OK
+    await Api.delete('education', object_id)
+    const res = await Api.get("educations", user_id);
+    setEducations(res.data);
+    setIsEditing(false);}
+
   return (
     <Card.Text>
       <Row className="align-items-center">
@@ -31,18 +43,7 @@ function EducationCard({ education, isEditable, setIsEditing, setEducations }) {
             <Button
               variant="outline-info"
               size="sm"
-              onClick={async (e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                
-                const user_id = education.user_id;
-                const object_id = education.object_id;
-                console.log(object_id) // 디버깅 //OK
-                await Api.delete('education/delete', object_id)
-                const res = await Api.get("educationlist", user_id);
-                setEducations(res.data);
-                setIsEditing(false);
-              }}
+              onClick={delHandler}
               className = "mr-3"  
             >삭제
             </Button>
@@ -54,3 +55,4 @@ function EducationCard({ education, isEditable, setIsEditing, setEducations }) {
 }
 
 export default EducationCard
+
