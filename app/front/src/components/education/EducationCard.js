5f4c9dfe24964c9e30d0022b1e9e0905e4cb400a
성현 배 <lydiabae0719@gmail.com>
2022-09-01 21:26:48 +0000

    디버깅 주석 제거

--- app/front/src/components/education/EducationCard.js
+++ app/front/src/components/education/EducationCard.js
@@ -9,7 +9,6 @@ function EducationCard({ education, isEditable, setIsEditing, setEducations}) {
     
     const user_id = education.user_id;
     const object_id = education.object_id;
-    console.log(object_id) // 디버깅 //OK
     await Api.delete('education', object_id)
     const res = await Api.get("educations", user_id);
     setEducations(res.data);
