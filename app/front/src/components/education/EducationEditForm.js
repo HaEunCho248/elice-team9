73e1c62c03835a0085c7a6bcdcc4b6fe5aaa219d
성현 배 <lydiabae0719@gmail.com>
2022-09-02 11:30:58 +0000

    날짜 수정...제발

--- app/front/src/components/education/EducationEditForm.js
+++ app/front/src/components/education/EducationEditForm.js
@@ -3,38 +3,39 @@ import { Button, Form, Col, Row } from "react-bootstrap";
 import * as Api from "../../api";
 import DatePicker from "react-datepicker";
 
-function ProjectEditForm({currentProject, setProjects, setIsEditing }) {
-  // useState로 title 상태를 생성함.
-  const [title, setTitle] = useState(currentProject.title);
-  // useState로 description 상태를 생성함.
-  const [description, setDescription] = useState(currentProject.description);
-  // useState로 startDate, endDate 상태를 생성함.
-  const [startDate, setStartDate] = useState(new Date(currentProject.startDate));
-  const [endDate, setEndDate] = useState(new Date(currentProject.endDate));
+
+function EducationEditForm({ currentEducation, setEducations, setIsEditing }) {
+  const [school, setSchool] = useState(currentEducation.school);
+  //useState로 description 상태를 생성함.
+  const [major, setMajor] = useState(currentEducation.major);
+  const [position, setPosition] = useState(currentEducation.position);
+  const [startDate, setStartDate] = useState(new Date(currentEducation.startDate));
+  const [endDate, setEndDate] = useState(new Date(currentEducation.endDate));
 
   const today = new Date();
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     e.stopPropagation();
 
-    // currentProject의 user_id를 user_id 변수에 할당함.
-    const object_id = currentProject.object_id;
-    const user_id = currentProject.user_id;
+    // currentAward의 user_id를 user_id 변수에 할당함.
+    const user_id = currentEducation.user_id;
+    const object_id = currentEducation.object_id;
 
-    // "project/object_id" 엔드포인트로 PUT 요청함.
-    await Api.put(`project/${currentProject.object_id}`, {
+    // "awards/수상 id" 엔드포인트로 PUT 요청함.
+    await Api.put(`education/${currentEducation.object_id}`, {
       object_id,
-      title,
-      description,
+      school,
+      major,
+      position,
       startDate, 
       endDate,
     });
 
-    // "project/유저id" 엔드포인트로 GET 요청함.
-    const res = await Api.get("projects", user_id);
-    // projects를 response의 data로 세팅함.
-    setProjects(res.data);
+    // "awardlist/유저id" 엔드포인트로 GET 요청함.
+    const res = await Api.get("educations", user_id);
+    // awards를 response의 data로 세팅함.
+    setEducations(res.data);
     // 편집 과정이 끝났으므로, isEditing을 false로 세팅함.
     setIsEditing(false);
   };
@@ -45,24 +46,22 @@ function ProjectEditForm({currentProject, setProjects, setIsEditing }) {
       <Form.Group controlId="formBasicTitle">
         <Form.Control
           type="text"
-          placeholder="프로젝트 내역"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          placeholder="학교명"
+          value={school}
+          onChange={(e) => setSchool(e.target.value)}
         />
       </Form.Group>
 
       <Form.Group controlId="formBasicDescription" className="mt-3">
         <Form.Control
           type="text"
-          placeholder="프로젝트 상세내역"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          placeholder="전공"
+          value={major}
+          onChange={(e) => setMajor(e.target.value)}
         />
       </Form.Group>
-
       <Form.Group as={Row} className="mt-3">
-        
-        <Col>
+      <Col>
           시작일 <DatePicker 
           selected={startDate}
           onChange={(date) => {
@@ -84,6 +83,53 @@ function ProjectEditForm({currentProject, setProjects, setIsEditing }) {
         </Col>
         
       </Form.Group>
+      <Form.Group>
+      <div key={`inline-radio`} className="mb-3 mt-3">
+        <Form.Check
+          inline
+          label="재학중"
+	        id="radio1"
+          type="radio"
+          name="position"
+          value="재학중"
+          checked={position === "재학중"}
+          onChange={(e) => setPosition(e.target.value)}
+        />
+        <Form.Check
+          inline
+          label="학사졸업"
+		  id="radio2"
+          type="radio"
+          name="position"
+          value="학사졸업"
+          checked={position === "학사졸업"}
+          onChange={(e) => setPosition(e.target.value)}
+
+        />
+        <Form.Check
+          inline
+          label="석사졸업"
+		  id="radio3"
+          type="radio"
+          name="position"
+          value="석사졸업"
+          checked={position === "석사졸업"}
+          onChange={(e) => setPosition(e.target.value)}
+        />
+        <Form.Check
+          inline
+          label="박사졸업"
+		  id="radio4"
+          type="radio"
+          name="position"
+          value="박사졸업"
+          checked={position === "박사졸업"}
+          onChange={(e) => setPosition(e.target.value)}
+
+        />
+      </div>
+
+      </Form.Group>
 
       <Form.Group as={Row} className="mt-3 text-center mb-4">
         <Col sm={{ span: 20 }}>
@@ -99,5 +145,6 @@ function ProjectEditForm({currentProject, setProjects, setIsEditing }) {
   );
 }
 
-export default ProjectEditForm;
+export default EducationEditForm;
+
 
